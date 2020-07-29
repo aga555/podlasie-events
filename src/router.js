68ec55398eff72244f6bb0dd6b7aea5b5c988258
akrawczyk <agammkrawczyk@gmail.com>
2020-07-29 18:19:46 +0200

    #nav bar

--- src/router.js
+++ src/router.js
@@ -1,10 +1,36 @@
-import HelloWorld from "@/components/HelloWorld";
+import Home from "@/components/Home";
+import Signup from "@/components/user/Signup";
+import Events from "@/components/events/Events";
+import CreateEvent from "@/components/events/CreateEvent";
+import Profile from "@/components/user/Profile";
 
 export const routers = [
     {
         path: '/',
-        component: HelloWorld
-    }
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/signup',
+        name: 'signup',
+        component: Signup
+    },
+
+    {
+        path: '/events',
+        name: 'events',
+        component: Events
+    },
+    {
+        path: '/createEvent',
+        name: 'createEvent',
+        component: CreateEvent
+    },
+    {
+        path: '/profile',
+        name: 'profile',
+        component: Profile
+    },
 ]
 
 
