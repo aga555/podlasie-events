302fde554750ecde4081a169df126da701dc348e
akrawczyk <agammkrawczyk@gmail.com>
2020-07-29 14:43:32 +0200

    #nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+        <v-container>
+
+
+            <v-app-bar
+                    color="green"
+                    dark
+                    prominent
+            >
+                <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+                <v-toolbar-title>Podlasie extra </v-toolbar-title>
+
+                <v-spacer></v-spacer>
+
+
+              <v-btn icon>
+                <v-icon>mdi-bell</v-icon>
+              </v-btn>
+
+                <v-btn icon>
+                    <v-icon>mdi-magnify</v-icon>
+                </v-btn>
+
+                <v-btn icon>
+                    <v-icon>mdi-filter</v-icon>
+                </v-btn>
+
+                <v-btn icon>
+                    <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+            </v-app-bar>
+
+            <v-navigation-drawer
+                    v-model="drawer"
+                    absolute
+                    bottom
+                    temporary
+            >
+                <v-list
+                        nav
+                        dense
+                >
+                    <v-list-item-group
+                            v-model="group"
+                            active-class="deep-purple--text text--accent-4"
+                    >
+                        <v-list-item>
+                            <v-list-item-title>Foo</v-list-item-title>
+                        </v-list-item>
+
+                        <v-list-item>
+                            <v-list-item-title>Bar</v-list-item-title>
+                        </v-list-item>
+
+                        <v-list-item>
+                            <v-list-item-title>Fizz</v-list-item-title>
+                        </v-list-item>
+
+                        <v-list-item>
+                            <v-list-item-title>Buzz</v-list-item-title>
+                        </v-list-item>
+                    </v-list-item-group>
+                </v-list>
+            </v-navigation-drawer>
+
+            <v-card-text>
+                The navigation drawer will appear from the bottom on smaller size screens.
+            </v-card-text>
+        </v-container>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+    export default {
+        name: 'App',
+        data() {
+            return {
+                drawer: false
+            }
+        }
+    }
+
+
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
